c60c7a412f1dba0fb05bc3c6316e64ac23b530e7
Alex <alexandrelachance@me.com>
2022-01-11 20:51:51 -0500

    Basic layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Loading from "./loading.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  isLoading: boolean;
+  emoji: string;
+  loading: any;
+}
+
+class App extends React.Component<{}, AppProps> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      emoji: "❔",
+      loading: <img className="Loading" src={Loading} alt="Loading icon." />,
+    };
+  }
+
+  click(): void {
+    if (this.state.isLoading) {
+      this.setState({ emoji: "." });
+    } else {
+      this.setState({ emoji: "❔" });
+    }
+    console.log(this.state.isLoading);
+
+    this.setState({ isLoading: !this.state.isLoading });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.loading}
+          <div className="Emoji">{this.state.emoji}</div>
+
+          <div className="Input">
+            <select name="Language" id="lang">
+              <option value="en">🇬🇧</option>
+              <option value="fr">🇫🇷</option>
+            </select>
+            <input type="text" placeholder="Search term" />
+            <button onClick={() => this.click()}>Calculate</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
